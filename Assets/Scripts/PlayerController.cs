8f13b49ddf4d87a5fdacb2dc2dfcf3591675e466
Andrew Yates-148003089 <148003089@vusdnoc.vusd.k12.ca.us>
2016-04-14 07:57:56 -0700

    Starting project with working multiplayer

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+
+    private PlayerMotor playerMotor;
+
+	// Use this for initialization
+	void Start ()
+    {
+		playerMotor = GetComponent<PlayerMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        /**
+        Movement
+         */
+		float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 horiMove = transform.right * xMov;
+        Vector3 vertMove = transform.forward * zMov;
+
+        Vector3 velocity = (horiMove + vertMove).normalized * speed;
+
+        playerMotor.Move(velocity);
+
+        /**
+        Rotation for camera
+         */
+        float xRot = Input.GetAxisRaw("Mouse Y");
+
+        float cameraRotation = xRot * lookSensitivity;
+
+        playerMotor.RotateCamera(cameraRotation);
+
+        /**
+        Rotation for player
+         */
+        float yRot = Input.GetAxisRaw("Mouse X");
+
+        float bodyRotation = yRot * lookSensitivity;
+
+        playerMotor.Rotate(new Vector3(0f, bodyRotation, 0f));
+	}
+}
