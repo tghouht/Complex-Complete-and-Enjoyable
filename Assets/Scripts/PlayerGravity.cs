88fe417a2776ef36ae7e42e437e6d5acb258f7fe
ad9836 <ad9836@aol.com>
2016-04-29 13:33:18 -0700

    Add Gravity

---
+++ Assets/Scripts/PlayerGravity.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerGravity : MonoBehaviour {
+
+
+	[HideInInspector]
+	public List<PlanetGravity> nearPlanets = new List<PlanetGravity>();
+
+	Rigidbody bodyrigid;
+	FirstPersonController controller;
+	
+	void Start(){
+		bodyrigid = GetComponent<Rigidbody> ();
+		bodyrigid.useGravity = false;
+		bodyrigid.constraints = RigidbodyConstraints.FreezeRotation;
+		controller = GetComponent<FirstPersonController>();
+	}
+
+	void FixedUpdate () {
+		if(nearPlanets.Count == 0)
+			return;
+		
+		Transform nearestPlanet = null;
+		float smallestDistance = int.MaxValue;
+		Vector3 force = Vector3.zero;
+		foreach (PlanetGravity planet in nearPlanets){
+			//Gets gravity vector of planet
+			force += planet.GetGravityForce(bodyrigid);
+
+			//Distance to each nearby Planet
+			RaycastHit ray = new RaycastHit();
+			Physics.Raycast(transform.position,(planet.transform.position - transform.position).normalized,out ray,float.MaxValue,controller.groundedMask);
+
+			//Keeps Track of nearest planet
+			if(ray.distance < smallestDistance){
+				smallestDistance = ray.distance;
+				nearestPlanet = planet.transform;
+			}
+		}
+
+		//Angles player towards nearest Planet
+		Vector3 gravityUp = (transform.position - nearestPlanet.position).normalized;
+		Quaternion rotation = Quaternion.FromToRotation(transform.up,gravityUp) * transform.rotation;
+		transform.rotation = Quaternion.Lerp(transform.rotation,rotation,Time.fixedDeltaTime * 2f);
+
+		//Applies Gravity
+		bodyrigid.AddForce(force);
+	}
+}
